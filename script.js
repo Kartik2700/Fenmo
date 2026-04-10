b9355666eaed6d5e6dcc7a48777084d027af68e4
Kartik2700 <ksharda2700@gmail.com~>
2026-04-10 21:36:36 +0530

    FIX - calling expense API from .js file

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
         };
 
         try {
-            const response = await fetch('http://localhost:8080/expenses', {
+            const response = await fetch('/expenses', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(payload)

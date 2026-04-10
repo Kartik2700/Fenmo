158851874c8595a73f5b3fcd2bb3721d3ed5ab19
Kartik2700 <ksharda2700@gmail.com~>
2026-04-10 19:05:34 +0530

    Created  GetExpenses to Retrieve all stored Expenses

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"sort"
 	"sync"
 	"time"
 )
@@ -36,12 +37,16 @@ func main() {
 	// Post endpoint to register the Expenses
 	mux.HandleFunc("POST /expenses", RegisterExpense)
 
+	// Get endpoint to fetch Expenses
+	mux.HandleFunc("GET /expenses", GetExpenses)
+
 	log.Println("Server starting on :8080 (In-Memory Mode)...")
 	if err := http.ListenAndServe(":8080", mux); err != nil {
 		log.Fatal(err)
 	}
 }
 
+// RegisterExpense to store the expense
 func RegisterExpense(w http.ResponseWriter, r *http.Request) {
 	var e Expense
 	if err := json.NewDecoder(r.Body).Decode(&e); err != nil {
@@ -59,4 +64,29 @@ func RegisterExpense(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(e)
+}
+
+// GetExpenses To Retrieve Expenses
+func GetExpenses(w http.ResponseWriter, r *http.Request) {
+	categoryFilter := r.URL.Query().Get("category")
+	sortOrder := r.URL.Query().Get("sort")
+
+	store.RLock()
+	defer store.RUnlock()
+	// Create a copy to avoid modifying the original during sorting/filtering
+	results := make([]Expense, 0)
+	for _, e := range store.expenses {
+		if categoryFilter == "" || e.Category == categoryFilter {
+			results = append(results, e)
+		}
+	}
+
+	if sortOrder == "date_desc" {
+		sort.Slice(results, func(i, j int) bool {
+			return results[i].Date > results[j].Date
+		})
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(results)
 }
